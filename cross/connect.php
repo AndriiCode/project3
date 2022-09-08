69cc82342aea8a17aad1d538a1562a0088803cfc
AndriiCode <112947733+AndriiCode@users.noreply.github.com>
2022-09-08 15:05:00 +0300

    Delete cross directory

--- cross/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "Admin";
-$password = "1111";
-$db_name = "cross";
-$db = mysqli_connect($host,$user,$password,$db_name) or die(mysqli_error());
-mysqli_set_charset ($db,"utf8");
-?>
